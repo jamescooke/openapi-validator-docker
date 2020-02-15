88eca80c92c350a50e06bb5968b32fa1009c45c5
James Cooke <jamescooke@users.noreply.github.com>
2020-02-15 17:18:47 +0000

    Refactor update script to generate outputs

--- update.sh
+++ update.sh
@@ -7,10 +7,21 @@
 
 set -eo pipefail
 
+# Output data in GitHub Actions format
+# $1 name
+# $2 value
+function output {
+    echo "::set-output name=$1::$2"
+}
+
+# === Check for version passed ===
+
 if [ -z "$1" ]; then
     echo "Please pass [NEW VERSION]"
     exit 1
 fi
+
+# === Check all tags ===
 
 echo ""
 echo "=== All tags ==="
@@ -20,9 +31,13 @@ existing_tag=$(git tag --list | grep "^$1$")
 set -e
 echo "* Existing tag = '$existing_tag'"
 if [ "$existing_tag" ]; then
-    echo "Exiting"
-    exit 1
+    output "found" "tag"
+    output "name" "$existing_tag"
+    output "make_pr" "no"
+    exit 0
 fi
+
+# === Check all branches ===
 
 echo ""
 echo "=== All branches ==="
@@ -32,14 +47,20 @@ existing_branch=$(git branch --list --all | grep "$1$")
 set -e
 echo "* Existing branch = '$existing_branch'"
 if [ "$existing_branch" ]; then
-    echo "Exiting"
-    exit 1
+    output "found" "branch"
+    output "name" "$existing_branch"
+    output "make_pr" "no"
+    exit 0
 fi
+
+# === Update Dockerfile ===
 
 version_without_prefix=${1//v/}
 echo ""
 echo "=== Updating Dockerfile with '$version_without_prefix' ==="
 
 sed --in-place "s/^ARG version=[0-9\.]*$/ARG version=$version_without_prefix/" Dockerfile
+
+output "make_pr" "yes"
 
 cat Dockerfile
